f589728cf85ea9247cedf9eee02547e5c5f54edc
Believit Ngwenya <believitngwenya@outlook.com>
2025-07-30 21:43:06 +0200

    Add files via upload

---
+++ plp week2.py.py
@@ -0,0 +1,21 @@
+# Dictionaries in Python
+
+my_list=[]
+print(my_list)
+my_list.append(10)
+print(my_list)
+my_list.append(20)
+my_list.append(30)
+my_list.append(40)
+
+print(my_list)
+my_list.insert(1,15)
+print(my_list)
+my_list.extend([50,60,70])
+print(my_list)
+my_list.pop(-1)
+print(my_list)
+my_list.sort()
+print(my_list)
+index_of_30 = my_list.index(30)
+print("Index of 30:", index_of_30) 
